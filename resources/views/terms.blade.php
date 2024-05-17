30651b315585eaedf8d495af2cb5ebbd458231be
AliciaCodes2 <tosin@techminded.com.ng>
2024-05-17 16:14:49 +0100

    first commit

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus placeat debitis officia provident distinctio,
+        sunt optio aut natus quod dolor!
+    </div>
+@endsection
